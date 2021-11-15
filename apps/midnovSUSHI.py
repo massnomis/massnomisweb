25a75ddb54563bc7239c2338987a80ac013f5dc4
Samuel Simons <massnomis@gmail.com>
2021-11-15 15:28:40 -0500

    kajs;ld

--- apps/midnovSUSHI.py
+++ apps/midnovSUSHI.py
@@ -10,8 +10,11 @@ def app():
    
 
 
-
-    df = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/204a4d8f-b1eb-4149-aefe-4d42c0da8e62/data/latest')
+    query_id = "204a4d8f-b1eb-4149-aefe-4d42c0da8e62"
+    df = pd.read_json(
+        f"https://api.flipsidecrypto.com/api/v2/queries/{query_id}/data/latest",
+    convert_dates=["TIMESTAMP_NTZ"],
+    )  
 
     t_f = False
     st.sidebar.write("Choose y-axis scale")
@@ -30,9 +33,9 @@ def app():
 
     df = px.scatter(
         df, #this is the dataframe you are trying to plot
-        x = "DAYZ",
+        x = "WEEK",
         y = "FROMM",
-        color = "PLATFORM",
+        color = "UNI_VS_SUSHI",
         orientation = "v",
         title = "1. Active Weekly Users",
         template = "plotly_white",
@@ -41,21 +44,92 @@ def app():
         log_y = t_f
     )
     st.plotly_chart(df)
-
-
-# MONTH	UNI_VS_SUSHI	USD_MONTH_SUM	MONTH_AVERAGE_USD
-    df = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/81b05fe3-5951-4c4c-91ae-b4a8fa20e5b8/data/latest')
-    df = px.bar(
+    query_id = "204a4d8f-b1eb-4149-aefe-4d42c0da8e62"
+    df = pd.read_json(
+        f"https://api.flipsidecrypto.com/api/v2/queries/{query_id}/data/latest",
+    convert_dates=["TIMESTAMP_NTZ"],
+    )  
+    df = px.scatter(
         df, #this is the dataframe you are trying to plot
-        x = "MONTH",
-        y = ["USD_MONTH_SUM","MONTH_AVERAGE_USD"],
+        x = "WEEK",
+        y = "TX_COUNT",
         color = "UNI_VS_SUSHI",
         orientation = "v",
-        title = "Monthly Swap Volume",      
+        title = "1. Active Weekly Users - TX count",
         template = "plotly_white",
         width = 1000,
         height = 600,
         log_y = t_f
     )
-    st.plotly_chart(df)    
+    st.plotly_chart(df)
+    query_id = "81b05fe3-5951-4c4c-91ae-b4a8fa20e5b8"
+    df = pd.read_json(
+        f"https://api.flipsidecrypto.com/api/v2/queries/{query_id}/data/latest",
+    convert_dates=["TIMESTAMP_NTZ"],
+    )  
+    df = px.scatter(
+        df, #this is the dataframe you are trying to plot
+        x = "MONTH",
+        y = "USD_MONTH_SUM",
+        color = "UNI_VS_SUSHI",
+        orientation = "v",
+        title = "2.  Monthly Swap Volume",
+        template = "plotly_white",
+        width = 1000,
+        height = 600,
+        log_y = t_f
+    )
+    st.plotly_chart(df)
+    query_id = "81b05fe3-5951-4c4c-91ae-b4a8fa20e5b8"
+    df = pd.read_json(
+        f"https://api.flipsidecrypto.com/api/v2/queries/{query_id}/data/latest",
+    convert_dates=["TIMESTAMP_NTZ"],
+    )  
+    df = px.scatter(
+        df, #this is the dataframe you are trying to plot
+        x = "MONTH",
+        y = "MONTH_AVERAGE_USD",
+        color = "UNI_VS_SUSHI",
+        orientation = "v",
+        title = "2.  Monthly Swap Volume-avg swap size",
+        template = "plotly_white",
+        width = 1000,
+        height = 600,
+        log_y = t_f
+    )
+    st.plotly_chart(df)
+    query_id = "81b05fe3-5951-4c4c-91ae-b4a8fa20e5b8"
+    df = pd.read_json(
+        f"https://api.flipsidecrypto.com/api/v2/queries/{query_id}/data/latest",
+    convert_dates=["TIMESTAMP_NTZ"],
+    )  
+    df = px.scatter(
+        df, #this is the dataframe you are trying to plot
+        x = "MONTH",
+        y = "TX_COUNT",
+        color = "UNI_VS_SUSHI",
+        orientation = "v",
+        title = "2.  Monthly Swap Volume-TX_COUNT",
+        template = "plotly_white",
+        width = 1000,
+        height = 600,
+        log_y = t_f
+    )
+    st.plotly_chart(df)
+    # MONTH_AVERAGE_USD
+# MONTH	UNI_VS_SUSHI	USD_MONTH_SUM	MONTH_AVERAGE_USD
+    # df = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/81b05fe3-5951-4c4c-91ae-b4a8fa20e5b8/data/latest')
+    # df = px.bar(
+    #     df, #this is the dataframe you are trying to plot
+    #     x = "MONTH",
+    #     y = ["USD_MONTH_SUM","MONTH_AVERAGE_USD"],
+    #     color = "UNI_VS_SUSHI",
+    #     orientation = "v",
+    #     title = "Monthly Swap Volume",      
+    #     template = "plotly_white",
+    #     width = 1000,
+    #     height = 600,
+    #     log_y = t_f
+    # )
+    # st.plotly_chart(df)    
 # # DHOUR	COUNT(POOL_NAME)	POOL_NAME
