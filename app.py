050f06693fe8d052b29b97f19511e2d38f353137
Samuel Simons <massnomis@gmail.com>
2021-11-23 00:34:34 +0200

    lkdsjf

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from apps import yeet
+
 import streamlit as st
+
+
 
 from multiapp import MultiApp
 
+
+
 from apps import MIDNOVLUNA,midnovSUSHI,THORCHAINMIDNOV,testt,levanaspecial,MINELUNA,earlynovTHOR,minestaking,defilamatry1,aave29,beth,FlipsideValidator,flashy,aaavemidoctoberbatch,thorchain,aave_migration,bluna,compare,eth_fees,eth_matic_vol,extra,GPdata,gpusers,home,loop1,loop2,polygon_fees,steth
 
+
+
 # import your app modules here
+
+
+
+
 
 
 
@@ -13,65 +23,131 @@ app = MultiApp()
 
 
 
+
+
+
+
 st.set_page_config(layout="wide")
+
+
 
 st.markdown("""
 
+
+
 # 
 
+
+
 """)
+
+
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 
 
+
+
+
+
 st.markdown("""
+
+
 
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
 
+
+
   <a class="navbar-brand" href="https://app.ens.domains/name/massnomis.eth/details" target="_blank">massnomis</a>
+
+
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 
+
+
     <span class="navbar-toggler-icon"></span>
+
+
 
   </button>
 
+
+
   <div class="collapse navbar-collapse" id="navbarNav">
+
+
 
     <ul class="navbar-nav">
 
+
+
       <li class="nav-item active">
+
+
 
         <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
 
+
+
       </li>
 
+
+
       <li class="nav-item">
+
+
 
         <a class="nav-link" href="https://github.com/massnomis/massnomisweb" target="_blank">GitHub</a>
 
+
+
       </li>
+
+
 
       <li class="nav-item">
 
+
+
         <a class="nav-link" href="https://twitter.com/sammycrypto4" target="_blank">Twitter</a>
+
+
 
       </li>
 
+
+
     </ul>
+
+
 
   </div>
 
+
+
 </nav>
+
+
 
 """, unsafe_allow_html=True)
 
 
 
+
+
+
+
 # Add all your application here
 
+
+
 app.add_app("home", home.app)
+
+
+
+
 
 
 
@@ -79,7 +155,15 @@ app.add_app("beth", beth.app)
 
 
 
+
+
+
+
 app.add_app("bluna", bluna.app)
+
+
+
+
 
 
 
@@ -87,7 +171,15 @@ app.add_app("compare", compare.app)
 
 
 
+
+
+
+
 app.add_app("eth_fees", eth_fees.app)
+
+
+
+
 
 
 
@@ -95,7 +187,15 @@ app.add_app("eth_matic_vol", eth_matic_vol.app)
 
 
 
+
+
+
+
 app.add_app("extra", extra.app)
+
+
+
+
 
 
 
@@ -103,7 +203,15 @@ app.add_app("GPdata", GPdata.app)
 
 
 
+
+
+
+
 app.add_app("gpusers", gpusers.app)
+
+
+
+
 
 
 
@@ -111,7 +219,15 @@ app.add_app("loop1", loop1.app)
 
 
 
+
+
+
+
 app.add_app("loop2", loop2.app)
+
+
+
+
 
 
 
@@ -119,7 +235,15 @@ app.add_app("polygon_fees", polygon_fees.app)
 
 
 
+
+
+
+
 app.add_app("steth", steth.app)
+
+
+
+
 
 
 
@@ -127,7 +251,15 @@ app.add_app("aave_migration", aave_migration.app)
 
 
 
+
+
+
+
 app.add_app("flashy", flashy.app)
+
+
+
+
 
 
 
@@ -135,7 +267,15 @@ app.add_app("thorchain", thorchain.app)
 
 
 
+
+
+
+
 app.add_app("aaavemidoctoberbatch", aaavemidoctoberbatch.app)
+
+
+
+
 
 
 
@@ -143,7 +283,15 @@ app.add_app("FlipsideValidator", FlipsideValidator.app)
 
 
 
+
+
+
+
 app.add_app("aave29", aave29.app)
+
+
+
+
 
 
 
@@ -151,7 +299,15 @@ app.add_app("defilamatry1", defilamatry1.app)
 
 
 
+
+
+
+
 app.add_app("minestaking", minestaking.app)
+
+
+
+
 
 
 
@@ -159,7 +315,15 @@ app.add_app("MINELUNA", MINELUNA.app)
 
 
 
+
+
+
+
 app.add_app("earlynovTHOR", earlynovTHOR.app)
+
+
+
+
 
 
 
@@ -167,7 +331,15 @@ app.add_app("levanaspecial", levanaspecial.app)
 
 
 
+
+
+
+
 app.add_app("testt", testt.app)
+
+
+
+
 
 
 
@@ -175,7 +347,15 @@ app.add_app("THORCHAINMIDNOV", THORCHAINMIDNOV.app)
 
 
 
+
+
+
+
 app.add_app("midnovSUSHI", midnovSUSHI.app)
+
+
+
+
 
 
 
@@ -185,9 +365,19 @@ app.add_app("MIDNOVLUNA", MIDNOVLUNA.app)
 
 
 
+
+
+
+
+
+
 # midnovSUSHI.py
 
+
+
 # aaavemidoctoberbatch
+
+
 
 # FlipsideValidator
 
@@ -197,6 +387,16 @@ app.add_app("MIDNOVLUNA", MIDNOVLUNA.app)
 
 
 
+
+
+
+
+
+
+
+
 # The main app
+
+
 
 app.run()
