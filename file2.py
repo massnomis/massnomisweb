a1108e3efbea11449af8eb8bcefbdc9de64dd490
Samuel Simons <massnomis@gmail.com>
2021-11-23 23:34:27 +0200

    lsdkj

--- file2.py
+++ file2.py
@@ -1,8 +1,15 @@
 import streamlit as st
-from extra import file3.py
+
+from extra import file3
+
 app = MultiApp()
+
 st.set_page_config(layout="wide")
+
 st.markdown("""
+
 # 
+
 """)
+
 app.add_app("file1.py",file1.py.app)
