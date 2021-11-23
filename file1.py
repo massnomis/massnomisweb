44fdc3aa8d56a9a2ff3a341c471b1833c42cc87b
Samuel Simons <massnomis@gmail.com>
2021-11-23 11:53:08 +0200

    ALKDSJLJAS

--- file1.py
+++ file1.py
@@ -1,2 +1,2 @@
-from apps import yeet
-app.add_app("yeet",yeet.app)
+
+df = px.bar(df,x = "DATE" , y = ETHEREUM_UNIQUE_WALLETS , title = "" , orientation = "v" , template = "plotly_white" ,  width = 1000, height = 600, log_y = t_f)
