ead0be21c80de25935966a0431f11de2b17710ea
massnomis <massnomis@gmail.com>
2021-11-24 04:51:56 -0500

    Add files via upload
    test

---
+++ builder_in_js/README.md
@@ -0,0 +1,13 @@
+# GenerateScript
+How to clone repository:
+
+git clone https://github.com/thehargun/GenerateScript.git
+
+-------------------------------------------------------------------------------------
+
+
+How to run the program:
+
+cd GenerateScript
+
+node main
---
+++ builder_in_js/main.js
@@ -0,0 +1,75 @@
+//package to create text document
+fs = require('fs');
+
+//variables of the python script
+const prompt = require('prompt-sync')();
+const id = prompt('Paste Query ID:');
+const title = prompt('Title:');
+const subheader = prompt('Subheader:');
+const layout = prompt('Layout:');
+const x = prompt('X Value:');
+const y = prompt('Y Value:');
+const color = prompt('Color:');
+
+//python script code template
+code = `import streamlit as st
+import plotly
+import plotly.express as px
+import pandas as pd
+import json
+
+def app():
+
+    #st.set_page_config(layout="${layout}")
+    st.title("${title}")
+
+    query_id = "${id}"
+    df = pd.read_json("https://api.flipsidecrypto.com/api/v2/queries/${id}/data/latest",
+    convert_dates=["TIMESTAMP_NTZ"],
+    )
+
+
+    t_f = False
+    st.sidebar.write("Choose y-axis scale")
+    check = st.sidebar.checkbox("Linear/Log")
+    if check: 
+        t_f = True
+    
+
+#-------------------------------------------------------
+    
+
+    st.dataframe(df)
+
+    st.markdown("""
+    """)
+    
+
+    
+
+
+    df = px.scatter(
+        df, #this is the dataframe you are trying to plot
+        x = "${x}",
+        y = "${y}",
+        // color = "${color}",
+        orientation = "v",
+        template = "plotly_white",
+        width = 1000,
+        height = 600,
+        log_y = t_f
+    )
+    st.plotly_chart(df)
+    // st.subheader('${subheader}')
+
+loop
+
+
+
+`
+
+//print template
+fs.writeFile(`${title}.txt`, code, function(err) {
+    if (err) return console.log(err);
+    console.log('done');
+});
