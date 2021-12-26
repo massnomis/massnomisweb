d9f0252c63058c053a4475e4cec7a418a537d385
Samuel Simons <massnomis@gmail.com>
2021-12-26 17:36:24 +0200

    last

--- updatedlvn.py
+++ updatedlvn.py
@@ -115,7 +115,48 @@ df = px.scatter(
     log_y = t_f
 )
 st.plotly_chart(df)
+df = pd.read_json("https://api.flipsidecrypto.com/api/v2/queries/9bdc0745-9d56-4be2-8a56-b4ebcdb10a9a/data/latest",
+convert_dates=["TIMESTAMP_NTZ"],
+)
 
+st.markdown("""
+METEORS
+""")
+
+df = px.scatter(
+    df, #this is the dataframe you are trying to plot
+    x = "DAY",
+    y = "RUNNING_TX_COUNT_SUM",
+    color = "RARITY",
+    orientation = "v",
+    template = "plotly_white",
+    width = 1000,
+    height = 600,
+    log_y = t_f
+)
+st.plotly_chart(df)
+df = pd.read_json("https://api.flipsidecrypto.com/api/v2/queries/9bdc0745-9d56-4be2-8a56-b4ebcdb10a9a/data/latest",
+convert_dates=["TIMESTAMP_NTZ"],
+)
+
+st.markdown("""
+METEORS
+""")
+
+df = px.scatter(
+    df, #this is the dataframe you are trying to plot
+    x = "DAY",
+    y = "RUNNING_VOLUME_SUM",
+    color = "RARITY",
+    orientation = "v",
+    template = "plotly_white",
+    width = 1000,
+    height = 600,
+    log_y = t_f
+)
+st.plotly_chart(df)
+# RUNNING_TX_COUNT_SUM
+# RUNNING_VOLUME_SUM
 # ------------------------
 df = pd.read_json("https://api.flipsidecrypto.com/api/v2/queries/430c8cd3-2a0d-4367-8656-03978589b7f1/data/latest",
 convert_dates=["TIMESTAMP_NTZ"],
@@ -221,8 +262,48 @@ df = px.scatter(
     log_y = t_f
 )
 st.plotly_chart(df)
+df = pd.read_json("https://api.flipsidecrypto.com/api/v2/queries/430c8cd3-2a0d-4367-8656-03978589b7f1/data/latest",
+convert_dates=["TIMESTAMP_NTZ"],
+)
 
+st.markdown("""
+EGGS
+""")
 
+df = px.bar(
+    df, #this is the dataframe you are trying to plot
+    x = "DAY",
+    y = "RUNNING_TX_COUNT_SUM",
+    color = "RARITY",
+    orientation = "v",
+    template = "plotly_white",
+    width = 1000,
+    height = 600,
+    log_y = t_f
+)
+st.plotly_chart(df)
+df = pd.read_json("https://api.flipsidecrypto.com/api/v2/queries/430c8cd3-2a0d-4367-8656-03978589b7f1/data/latest",
+convert_dates=["TIMESTAMP_NTZ"],
+)
+
+st.markdown("""
+EGGS
+""")
+
+df = px.bar(
+    df, #this is the dataframe you are trying to plot
+    x = "DAY",
+    y = "RUNNING_VOLUME_SUM",
+    color = "RARITY",
+    orientation = "v",
+    template = "plotly_white",
+    width = 1000,
+    height = 600,
+    log_y = t_f
+)
+st.plotly_chart(df)
+# RUNNING_TX_COUNT_SUM
+# RUNNING_VOLUME_SUM
 # ----------------------
 df = pd.read_json("https://api.flipsidecrypto.com/api/v2/queries/7e5ed5f7-8195-4f1d-b20c-a77f464b28e9/data/latest",
 convert_dates=["TIMESTAMP_NTZ"],
@@ -328,8 +409,49 @@ df = px.scatter(
     log_y = t_f
 )
 st.plotly_chart(df)
+df = pd.read_json("https://api.flipsidecrypto.com/api/v2/queries/7e5ed5f7-8195-4f1d-b20c-a77f464b28e9/data/latest",
+convert_dates=["TIMESTAMP_NTZ"],
+)
 
+st.markdown("""
+DUST
+""")
 
+df = px.bar(
+    df, #this is the dataframe you are trying to plot
+    x = "DAY",
+    y = "RUNNING_TX_COUNT_SUM",
+    color = "RARITY",
+    orientation = "v",
+    template = "plotly_white",
+    width = 1000,
+    height = 600,
+    log_y = t_f
+)
+st.plotly_chart(df)
+df = pd.read_json("https://api.flipsidecrypto.com/api/v2/queries/7e5ed5f7-8195-4f1d-b20c-a77f464b28e9/data/latest",
+convert_dates=["TIMESTAMP_NTZ"],
+)
+
+st.markdown("""
+DUST
+""")
+
+df = px.bar(
+    df, #this is the dataframe you are trying to plot
+    x = "DAY",
+    y = "RUNNING_VOLUME_SUM",
+    color = "RARITY",
+    orientation = "v",
+    template = "plotly_white",
+    width = 1000,
+    height = 600,
+    log_y = t_f
+)
+st.plotly_chart(df)
+
+# RUNNING_TX_COUNT_SUM
+# RUNNING_VOLUME_SUM
 
 # 9bdc0745-9d56-4be2-8a56-b4ebcdb10a9a
 # day, RARITY, COL_NAME, sum(TXN_COUNT),	sum(TXN_VOLUME), 	avg(TXN_AVG), median(TXN_MEDIAN),	min(FLOOR_PRICE)
